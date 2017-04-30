b5def9d4671bd5fc60e583ae924fb8e5a0614f6f
David Johansson <davidjo2@kth.se>
2017-04-30 23:05:27 +0200

    Added spaceship image

--- core/src/com/game/MainMenu.java
+++ core/src/com/game/MainMenu.java
@@ -1,6 +1,8 @@
 package com.game;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 
 public class MainMenu implements Screen {
@@ -22,7 +24,20 @@ public class MainMenu implements Screen {
     }
 
     public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        camera.update();
+        game.batch.setProjectionMatrix(camera.combined);
+
+        game.batch.begin();
+        game.font.draw(game.batch, "Welcome to Drop!!! ", 100, 150);
+        game.font.draw(game.batch, "Tap anywhere to begin!", 100, 100);
+        game.batch.end();
+
+        if (Gdx.input.isTouched()) {
+            dispose();
+        }
     }
 
     public void resize(int width, int height) {
