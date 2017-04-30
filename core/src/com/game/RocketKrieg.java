b78579fb4f5999df10cd38aab810c10cc6b56830
Johan <Johvh@kth.se>
2017-04-30 19:03:02 +0200

    Added a camera to the RocketKrieg class. Can be modified to follow playership in the future.

--- core/src/com/game/RocketKrieg.java
+++ core/src/com/game/RocketKrieg.java
@@ -3,15 +3,19 @@ package com.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.game.objects.PlayerSpaceShip;
 
 public class RocketKrieg implements Screen {
 	private final GameEntry game;
 	private PlayerSpaceShip ship;
+	private OrthographicCamera camera;
 
 	public RocketKrieg(final GameEntry game) {
 		this.game = game;
 		ship = new PlayerSpaceShip();
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
 	}
 
 	/**
@@ -28,6 +32,8 @@ public class RocketKrieg implements Screen {
 	public void render(float delta) {
 		Gdx.gl.glClearColor(0, 0, 0, 0);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		camera.update();
+		game.batch.setProjectionMatrix(camera.combined);
 		ship.update(delta);
 		game.batch.begin();
 		ship.render(game.batch);
