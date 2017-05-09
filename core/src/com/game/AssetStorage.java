738ac971a149d87b64ab5760eeec21f174154518
David Johansson <davidjo2@kth.se>
2017-05-09 13:48:07 +0200

    Flame animations now more realistic

--- core/src/com/game/AssetStorage.java
+++ core/src/com/game/AssetStorage.java
@@ -11,6 +11,7 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
  */
 public class AssetStorage {
     public static final Sprite spaceship = new Sprite(new Texture(Gdx.files.internal("images/spaceship/Spaceship.png")));
+    public static final Sprite missile = new Sprite(new Texture("images/spaceship/missile.png"));
     public static final Sprite tile1 = new Sprite(new Texture(Gdx.files.internal("images/worldGeneration/testTile.png")));
     public static final Sprite tile2 = new Sprite(new Texture(Gdx.files.internal("images/worldGeneration/testTile1.png")));
     public static final Sprite asteroid = new Sprite(new Texture("images/asteroid/Asteroid.png"));
