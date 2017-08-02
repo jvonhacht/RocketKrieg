469a81c9d7ed45fbc96ddff1d62bd15b525ba5d1
JohanvonHacht <JohanKJIP@gmail.com>
2017-08-02 19:48:33 +0200

    Updated special alien tile spawn

--- core/src/com/game/worldGeneration/Tile.java
+++ core/src/com/game/worldGeneration/Tile.java
@@ -33,7 +33,7 @@ public class Tile {
         tileEntities = new ArrayList<Entity>();
         int percent = rand.nextInt(100);
 
-        //choose random entity
+        //choose random entity.
         if(noise <= 0.4){
             for (int i = 0; (i<(RocketKrieg.getScore()/10) && i<3) || i<1; i++) {
                 tileEntities.add(new Asteroid(x + rand.nextInt(TILE_SIZE), y + rand.nextInt(TILE_SIZE)));
